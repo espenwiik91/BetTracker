04a7c352c94954a09a82b1b3f68a9cedc5925a28
espenwiik91 <35593150+espenwiik91@users.noreply.github.com>
2020-06-01 22:27:12 +0200

    Add files via upload

---
+++ BettingPort/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void setBet ()
+{
+    double odds;
+    double stake;
+
+    string betType [256] = {"BTTS", "HOME", "AWAY", "DRAW", "HT/FT", "GOAL SCORER", "NEXT GOAL SCORER", "CARD", "HOME TEAM CARDS", "AWAY CARDS"};
+
+    string betTipster [256] = {"Sjakktrekk", "Big Brains", "STAHObettor"};
+
+    string sport [256] = {"Football", "Basketball", "E-Sports", "MMA"};
+
+
+
+
+}
+
+int main()
+{
+
+
+}
